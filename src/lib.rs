8a968b1118e10c823460dcf5d25be2b7b940da84
polymonster <alexandercdixon@gmail.com>
2022-02-26 11:04:53 +0000

    - started imgui module

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@ pub mod gfx;
 /// Image reading/writing module support for (png, jpg, bmp, tiff, dds)
 pub mod image;
 
+/// Imgui rendering and platform implementation using imgui_sys
+pub mod imgui;
+
 /// Use bitmask for flags
 #[macro_use]
 extern crate bitflags;
