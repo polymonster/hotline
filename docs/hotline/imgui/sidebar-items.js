40057e5ad8db1f30794eb75538c519a25f4cd1ca
polymonster <alexandercdixon@gmail.com>
2022-03-19 16:14:11 +0000

    - rm docs (temp)

--- docs/hotline/imgui/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"fn":[["ImGuiPlatformIO_Set_Platform_GetWindowPos",""],["ImGuiPlatformIO_Set_Platform_GetWindowSize",""]],"struct":[["ImGui",""],["ImGuiInfo",""]],"type":[["WindowSizeCallback",""]]});
