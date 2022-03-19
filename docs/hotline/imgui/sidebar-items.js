d1905110ac3657190d1fadcf65d901bc6fffd05d
polymonster <alexandercdixon@gmail.com>
2022-03-19 16:22:17 +0000

    - re-add docs

---
+++ docs/hotline/imgui/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["ImGuiPlatformIO_Set_Platform_GetWindowPos",""],["ImGuiPlatformIO_Set_Platform_GetWindowSize",""]],"struct":[["ImGui",""],["ImGuiInfo",""]],"type":[["WindowSizeCallback",""]]});
