b5668697623f70ee845f06e3c78daa9b106c286a
polymonster <alexandercdixon@gmail.com>
2022-03-19 16:32:02 +0000

    -re-add docs now without any lfs tracking, so images work

---
+++ docs/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["hotline"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"hotline/gfx/struct.AdapterInfo.html\" title=\"struct hotline::gfx::AdapterInfo\">AdapterInfo</a>","synthetic":false,"types":["hotline::gfx::AdapterInfo"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
