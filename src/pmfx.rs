866f774f288ff4b2264124c5c666972f19a1454f
polymonster <alexandercdixon@gmail.com>
2022-06-03 17:12:39 +0100

    - pmfx wip

--- src/pmfx.rs
+++ src/pmfx.rs
@@ -72,6 +72,35 @@ pub struct Pmfx<'a, D: gfx::Device> {
     pipelines: Vec<gfx::RenderPipelineInfo<'a, D>>
 }
 
+const SEMANTIC_NAMES : [&str; 9] = [
+    "SV_POSITION",
+    "POSITION",
+    "TEXCOORD",
+    "NORMAL",
+    "TANGENT",
+    "BITANGENT",
+    "BLENDWEIGHTS",
+    "COLOR",
+    "BLENDINDICES"
+];
+
+fn get_format_for_semantic(semantic_size: u32, num_elements: u32) -> gfx::Format {
+    match semantic_size {
+        4 => match num_elements {
+            1 => gfx::Format::R32f,
+            2 => gfx::Format::RG32f,
+            3 => gfx::Format::RGB32f,
+            4 => gfx::Format::RGBA32f,
+            _ => panic!("hotline::pmfx: unsupported vertex element size!")
+        }
+        1 => match num_elements {
+            4 => gfx::Format::RGBA8u,
+            _ => panic!("hotline::pmfx: unsupported vertex element size!")
+        }
+        _ => panic!("hotline::pmfx: unsupported sematic size!")
+    }
+}
+
 impl<'a, D> Pmfx<'a, D> where D: gfx::Device {
     pub fn create() -> Self {
         Pmfx {
@@ -83,7 +112,22 @@ impl<'a, D> Pmfx<'a, D> where D: gfx::Device {
         let pmfx_data = fs::read(filepath).unwrap();
         let shader: PmfxShader = serde_json::from_slice(&pmfx_data).unwrap();
 
-        let a = 0;
+        for technique in shader.techniques {
+            let mut input_layout : Vec<gfx::InputElementInfo> = Vec::new();
+            for input in technique.vs_inputs {
+                input_layout.push(gfx::InputElementInfo {
+                    semantic: String::from(SEMANTIC_NAMES[input.semantic_id as usize]),
+                    index: input.semantic_index,
+                    format: get_format_for_semantic(input.element_size, input.num_elements),
+                    input_slot: input.semantic_index,
+                    aligned_byte_offset: input.offset,
+                    input_slot_class: gfx::InputSlotClass::PerVertex,
+                    step_rate: 0,
+                });
+            }
+
+            let a = 0;
+        }
     }
 
     pub fn load_pipeline(&mut self) {
