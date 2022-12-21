531c28fdd66c500bd5dec313cc8c2abe7f7a11fa
polymonster <alexandercdixon@gmail.com>
2022-12-21 14:56:00 +0000

    - rm docs and imgui-sys (now using the crates.io package)

--- docs/hotline/gfx/d3d12/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Buffer",""],["CmdBuf",""],["ComputePipeline",""],["Device",""],["Heap",""],["ReadBackRequest",""],["RenderPass",""],["RenderPipeline",""],["Shader",""],["SwapChain",""],["Texture",""]]});
