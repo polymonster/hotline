180cfa91a94fcafac5c4a9b6a995bd4ccc1f4d48
polymonster <alexandercdixon@gmail.com>
2023-01-05 20:12:18 +0000

    - testing multiple draw calls, figured out a few improvements using bevy stage sets to stablise render order

--- examples/geometry/main.rs
+++ examples/geometry/main.rs
@@ -39,6 +39,9 @@ struct Velocity {
 }
 
 #[derive(Component)]
+struct WorldMatrix(Mat4f);
+
+#[derive(Component)]
 struct Rotation(Vec3f);
 
 #[derive(Component)]
@@ -146,47 +149,54 @@ fn update_cameras(
     let app = &app.res;
     for (mut position, mut rotation, mut view_proj) in &mut query {
 
-        // get keyboard position movement
-        let keys = app.get_keys_down();
-        let mut cam_move_delta = Vec3f::zero();
-        if keys['A' as usize] {
-            cam_move_delta.x -= 1.0;
-        }
-        if keys['D' as usize] {
-            cam_move_delta.x += 1.0;
-        }
-        if keys['Q' as usize] {
-            cam_move_delta.y -= 1.0;
-        }
-        if keys['E' as usize] {
-            cam_move_delta.y += 1.0;
-        }
-        if keys['W' as usize] {
-            cam_move_delta.z -= 1.0;
-        }
-        if keys['S' as usize] {
-            cam_move_delta.z += 1.0;
-        }
+        if main_window.res.is_focused() {
+            // get keyboard position movement
+            let keys = app.get_keys_down();
+            let mut cam_move_delta = Vec3f::zero();
+            if keys['A' as usize] {
+                cam_move_delta.x -= 1.0;
+            }
+            if keys['D' as usize] {
+                cam_move_delta.x += 1.0;
+            }
+            if keys['Q' as usize] {
+                cam_move_delta.y -= 1.0;
+            }
+            if keys['E' as usize] {
+                cam_move_delta.y += 1.0;
+            }
+            if keys['W' as usize] {
+                cam_move_delta.z -= 1.0;
+            }
+            if keys['S' as usize] {
+                cam_move_delta.z += 1.0;
+            }
 
-        // get mouse rotation
-        if app.get_mouse_buttons()[os::MouseButton::Left as usize] {
-            let mouse_delta = app.get_mouse_pos_delta();
-            rotation.0.x -= mouse_delta.y as f32;
-            rotation.0.y -= mouse_delta.x as f32;
+            // get mouse rotation
+            if app.get_mouse_buttons()[os::MouseButton::Left as usize] {
+                let mouse_delta = app.get_mouse_pos_delta();
+                rotation.0.x -= mouse_delta.y as f32;
+                rotation.0.y -= mouse_delta.x as f32;
+            }
+
+            // construct rotation matrix
+            let mat_rot_x = Mat4f::from_x_rotation(f32::deg_to_rad(rotation.0.x));
+            let mat_rot_y = Mat4f::from_y_rotation(f32::deg_to_rad(rotation.0.y));
+            let mat_rot = mat_rot_y * mat_rot_x;
+
+            // move relative to facing directions
+            position.pos += mat_rot * cam_move_delta;
         }
-
-        // construct rotation matrix
-        let mat_rot_x = Mat4f::from_x_rotation(f32::deg_to_rad(rotation.0.x));
-        let mat_rot_y = Mat4f::from_y_rotation(f32::deg_to_rad(rotation.0.y));
-        let mat_rot = mat_rot_y * mat_rot_x;
-
-        // move relative to facing directions
-        position.pos += mat_rot * cam_move_delta;
 
         // generate proj matrix
         let window_rect = main_window.res.get_viewport_rect();
         let aspect = window_rect.width as f32 / window_rect.height as f32;
         let proj = Mat4f::create_perspective_projection_lh_yup(f32::deg_to_rad(60.0), aspect, 0.1, 100000.0);
+
+        // construct rotation matrix
+        let mat_rot_x = Mat4f::from_x_rotation(f32::deg_to_rad(rotation.0.x));
+        let mat_rot_y = Mat4f::from_y_rotation(f32::deg_to_rad(rotation.0.y));
+        let mat_rot = mat_rot_y * mat_rot_x;
 
         // build view / proj matrix
         let translate = Mat4f::from_translation(position.pos);
@@ -288,42 +298,43 @@ fn render_grid(
 }
 
 fn render_cube(
-    main_window: ResMut<MainWindowRes>,
     mut swap_chain: ResMut<SwapChainRes>, 
-    mut device: ResMut<DeviceRes>,
     mut cmd_buf: ResMut<CmdBufRes>,
-    mut imdraw: ResMut<ImDrawRes>,
+    main_window: ResMut<MainWindowRes>,
     cube_mesh: ResMut<MeshRes>,
     pmfx: ResMut<PmfxRes>,
-    mut query: Query<&ViewProjectionMatrix> ) {
+    mut query: Query<&ViewProjectionMatrix>,
+    mut query2: Query<&WorldMatrix> ) {
+
+    // unpack
+    let cmd_buf = &mut cmd_buf.res;
+    let swap_chain = &mut swap_chain.res;
+    let main_window = &main_window.res;
+    let pmfx = &pmfx.res;
+
+    let window_rect = main_window.get_viewport_rect();
+    let viewport = gfx::Viewport::from(window_rect);
+    let scissor = gfx::ScissorRect::from(window_rect);
+
+    // setup pass
+    cmd_buf.begin_render_pass(swap_chain.get_backbuffer_pass_no_clear_mut());
+    cmd_buf.set_viewport(&viewport);
+    cmd_buf.set_scissor_rect(&scissor);
+    cmd_buf.set_render_pipeline(&pmfx.get_render_pipeline("imdraw_mesh").unwrap());
 
     for view_proj in &mut query {
-        // render grid
-        let cmd_buf = &mut cmd_buf.res;
-        let swap_chain = &mut swap_chain.res;
-        let main_window = &main_window.res;
-        let imdraw = &mut imdraw.res;
-        let pmfx = &pmfx.res;
-
-        let draw_bb = swap_chain.get_backbuffer_index() as usize;
-        let window_rect = main_window.get_viewport_rect();
-        let viewport = gfx::Viewport::from(window_rect);
-        let scissor = gfx::ScissorRect::from(window_rect);
-
-        cmd_buf.begin_render_pass(swap_chain.get_backbuffer_pass_no_clear_mut());
-        cmd_buf.set_viewport(&viewport);
-        cmd_buf.set_scissor_rect(&scissor);
-
-        cmd_buf.set_render_pipeline(&pmfx.get_render_pipeline("imdraw_mesh").unwrap());
         cmd_buf.push_constants(0, 16, 0, &view_proj.0);
-
-        // drw here
-        cmd_buf.set_index_buffer(&cube_mesh.res.ib);
-        cmd_buf.set_vertex_buffer(&cube_mesh.res.vb, 0);
-        cmd_buf.draw_indexed_instanced(cube_mesh.res.num_indices, 1, 0, 0, 0);
-
-        cmd_buf.end_render_pass();
+        for world_matrix in & mut query2 {
+            // draw
+            cmd_buf.push_constants(1, 16, 0, &world_matrix.0);
+            cmd_buf.set_index_buffer(&cube_mesh.res.ib);
+            cmd_buf.set_vertex_buffer(&cube_mesh.res.vb, 0);
+            cmd_buf.draw_indexed_instanced(cube_mesh.res.num_indices, 1, 0, 0, 0);
+        }
     }
+
+    // end
+    cmd_buf.end_render_pass();
 }
 
 struct Vertex {
@@ -515,12 +526,12 @@ fn create_cube_mesh(dev: &mut gfx_platform::Device) -> Mesh {
     ];
 
     let indices: Vec<u16> = vec![
-        0,  1,  2,  2,  3,  0,   // front face
-        4,  5,  6,  6,  7,  4,   // back face
-        8,  9,  10, 10, 11, 8,   // right face
-        12, 13, 14, 14, 15, 12,  // left face
-        16, 17, 18, 18, 19, 16,  // top face
-        20, 21, 22, 22, 23, 20   // bottom face
+        0,  2,  1,  2,  0,  3,   // front face
+        4,  6,  5,  6,  4,  7,   // back face
+        8,  10, 9,  10, 8,  11,  // right face
+        12, 14, 13, 14, 12, 15,  // left face
+        16, 18, 17, 18, 16, 19,  // top face
+        20, 22, 21, 22, 20, 23   // bottom face
     ];
 
     Mesh {
@@ -562,14 +573,10 @@ fn render_imgui(
 pub struct StageUpdate;
 
 #[derive(StageLabel)]
-pub struct StageRender3D;
+pub struct StageRender;
 
 #[derive(StageLabel)]
 pub struct StageRender2D;
-
-#[derive(StageLabel)]
-pub struct StageRenderImGui;
-
 
 fn main() -> Result<(), hotline_rs::Error> {    
 
@@ -617,6 +624,13 @@ fn main() -> Result<(), hotline_rs::Error> {
     world.spawn((
         Position { pos: Vec3f::zero() },
         Velocity { vel: Vec3f::one() },
+        WorldMatrix { 0: Mat4f::from_translation(Vec3f::zero()) }
+    ));
+
+    world.spawn((
+        Position { pos: Vec3f::zero() },
+        Velocity { vel: Vec3f::one() },
+        WorldMatrix { 0: Mat4f::from_translation(Vec3f::unit_z() * 2.0) }
     ));
     
     world.spawn((
@@ -626,7 +640,6 @@ fn main() -> Result<(), hotline_rs::Error> {
         Camera,
     ));
 
-    let mut mode = 0;
     let mut imgui_open = true;
     let mut call_render_2d = false;
     let mut call_render_3d = true;
@@ -639,11 +652,6 @@ fn main() -> Result<(), hotline_rs::Error> {
         ctx.main_window.update(&mut ctx.app);
 
         // hotline update
-        let keys = ctx.app.get_keys_down();
-        if keys['M' as usize] {
-            mode = !mode;
-        }
-
         imgui.new_frame(&mut ctx.app, &mut ctx.main_window, &mut ctx.device);
         if imgui.begin("hello world", &mut imgui_open, imgui::WindowFlags::NONE) {
             imgui.checkbox("Render 3D", &mut call_render_3d);
@@ -683,21 +691,25 @@ fn main() -> Result<(), hotline_rs::Error> {
             .with_system(movement)
         );
 
-        if call_render_3d {
-            schedule.add_stage(StageRender3D, SystemStage::single_threaded()
+        schedule.add_stage(StageRender, SystemStage::single_threaded()
+            .with_system_set(
+                SystemSet::new().label("render_debug_3d")
                 .with_system(render_grid)
-                .with_system(render_cube)
-            );
-        }
-
-        if call_render_2d {
-            schedule.add_stage(StageRender2D, SystemStage::single_threaded()
-                .with_system(render_2d)
-            );
-        }
-
-        schedule.add_stage(StageRenderImGui, SystemStage::single_threaded()
-            .with_system(render_imgui)
+            )
+            .with_system_set(
+                SystemSet::new().label("render_main")
+                .with_system(render_cube).after("render_debug_3d")
+            )
+            /*
+            .with_system_set(
+                SystemSet::new().label("render_debug_2d")
+                .with_system(render_2d).after("render_main")
+            )
+            */
+            .with_system_set(
+                SystemSet::new().label("render_imgui")
+                .with_system(render_imgui).after("render_main")
+            )
         );
 
         // run systems
