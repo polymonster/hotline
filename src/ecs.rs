998d57af5925058ce60703cbc6c7b1533796a2ed
polymonster <alexandercdixon@gmail.com>
2023-01-18 17:41:21 +0000

    - major stability improvements for code hot-reloading

--- src/ecs.rs
+++ src/ecs.rs
@@ -64,3 +64,26 @@ pub struct Camera;
 
 #[derive(Component)]
 pub struct MeshComponent(pub pmfx::Mesh<gfx_platform::Device>);
+
+
+pub fn add_world_resources(world: &mut World, ctx: Context<gfx_platform::Device, os_platform::App>) {
+    // move hotline resource into world
+    world.insert_resource(DeviceRes {0: ctx.device});
+    world.insert_resource(AppRes {0: ctx.app});
+    world.insert_resource(MainWindowRes {0: ctx.main_window});
+    world.insert_resource(PmfxRes {0: ctx.pmfx});
+    world.insert_resource(ImDrawRes {0: ctx.imdraw});
+    world.insert_resource(ImGuiRes {0: ctx.imgui});
+}
+
+pub fn remove_world_resources(world: &mut World, mut ctx: Context<gfx_platform::Device, os_platform::App>)  
+    -> Context<gfx_platform::Device, os_platform::App> {
+    // move resources back out into ctx
+    ctx.device = world.remove_resource::<DeviceRes>().unwrap().0;
+    ctx.app = world.remove_resource::<AppRes>().unwrap().0;
+    ctx.main_window = world.remove_resource::<MainWindowRes>().unwrap().0;
+    ctx.pmfx = world.remove_resource::<PmfxRes>().unwrap().0;
+    ctx.imdraw = world.remove_resource::<ImDrawRes>().unwrap().0;
+    ctx.imgui = world.remove_resource::<ImGuiRes>().unwrap().0;
+    ctx
+}
