4ed6c0805b944f7cbdf4d80a7ea63fdf953b467a
polymonster <alexandercdixon@gmail.com>
2023-01-22 15:01:05 +0000

    - adjustments to make data build and first time build easier

--- build.rs
+++ build.rs
@@ -1,13 +1,15 @@
 use std::process::Command;
 
-fn main() {
-    if std::env::var("CARGO_FEATURE_BUILD_DATA").is_ok() {
-        let output = Command::new("bin/win32/pmbuild.exe")
-        .arg("win32")
-        .output()
-        .expect("pmbuild failed");
+#[cfg(target_os = "windows")]
+fn build_data() {
+    let output = Command::new("bin/win32/pmbuild.exe")
+    .arg("win32")
+    .output()
+    .expect("pmbuild failed");
+    println!("{}", String::from_utf8(output.stdout).unwrap());
+    println!("{}", String::from_utf8(output.stderr).unwrap());
+}
 
-        println!("{}", String::from_utf8(output.stdout).unwrap());
-        println!("{}", String::from_utf8(output.stderr).unwrap());
-    }
+fn main() {
+    build_data()
 }
