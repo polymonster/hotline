569331add696fd7148a22a4daac80f4dfcb29c96
polymonster <alexandercdixon@gmail.com>
2023-01-22 15:53:55 +0000

    - omit binaries from crates

--- build.rs
+++ build.rs
@@ -2,12 +2,14 @@ use std::process::Command;
 
 #[cfg(target_os = "windows")]
 fn build_data() {
-    let output = Command::new("bin/win32/pmbuild.exe")
-    .arg("win32")
-    .output()
-    .expect("pmbuild failed");
-    println!("{}", String::from_utf8(output.stdout).unwrap());
-    println!("{}", String::from_utf8(output.stderr).unwrap());
+    if std::env::var("CARGO_FEATURE_BUILD_DATA").is_ok() {
+        let output = Command::new("bin/win32/pmbuild.exe")
+        .arg("win32")
+        .output()
+        .expect("pmbuild failed");
+        println!("{}", String::from_utf8(output.stdout).unwrap());
+        println!("{}", String::from_utf8(output.stderr).unwrap());
+    }
 }
 
 fn main() {
